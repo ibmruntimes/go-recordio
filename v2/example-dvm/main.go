d3b15795b66512ee7dc3dba3550eabf78eb8c06a
Hyder Naqvi <hyder.naqvi@ibm.com>
2025-04-10 03:24:25 -0400

    Go fmt DVM Example

--- v2/example-dvm/main.go
+++ v2/example-dvm/main.go
@@ -21,12 +21,12 @@ type PLIST struct {
 
 // SQLVar represents a variable in the SQLDA structure
 type SQLVar struct {
-	SQLTYPE int16
-	SQLLEN  int16
-	SQLDATA int32
-	SQLIND  int32
-	SQLNAME_LENGTH  int16
-	Data    [30]byte
+	SQLTYPE        int16
+	SQLLEN         int16
+	SQLDATA        int32
+	SQLIND         int32
+	SQLNAME_LENGTH int16
+	Data           [30]byte
 }
 
 // SQLDA represents the SQLDA structure used for Metadata
